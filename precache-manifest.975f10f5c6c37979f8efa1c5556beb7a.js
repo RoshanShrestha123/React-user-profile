b68da163c2a44a2af38bbcb4cf7ab035b5b13cf4
roshan shrestha <roshan.shrestha4u@gmail.com>
2020-01-08 13:24:16 +0545

    Updates

---
+++ precache-manifest.975f10f5c6c37979f8efa1c5556beb7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59eccafafca89e24172cf5c697c260e",
+    "url": "/React-user-profile/index.html"
+  },
+  {
+    "revision": "976d80c37089cd6393b4",
+    "url": "/React-user-profile/static/css/main.27e625d9.chunk.css"
+  },
+  {
+    "revision": "475550e787f06892048c",
+    "url": "/React-user-profile/static/js/2.b20a97aa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-user-profile/static/js/2.b20a97aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "976d80c37089cd6393b4",
+    "url": "/React-user-profile/static/js/main.e517916b.chunk.js"
+  },
+  {
+    "revision": "64b427ea213f96a1a2d1",
+    "url": "/React-user-profile/static/js/runtime-main.0b89ec8e.js"
+  }
+]);
